78654abf07a618056b0dd778a6e703afe1c45ea3
ACZH05 <alfredchin2005@gmail.com>
2026-05-05 01:12:24 +0800

    Add parsing for request line

--- src/server.c
+++ src/server.c
@@ -4,11 +4,34 @@
 
 #define BUFFER_SIZE 4096
 
+
+int parseRequestLine(char *buffer, RequestLine *request_line) {
+	Slice *queue[] = {&request_line->method, &request_line->path, &request_line->version};
+	int cursor = 0;
+
+	for (int i = 0; i < 3; i++) {
+		queue[i]->start = &buffer[cursor];
+		queue[i]->len = 0;
+
+		while (buffer[cursor] != ' ' && buffer[cursor] != '\r') {
+			queue[i]->len += 1;
+			cursor += 1;
+		}
+		cursor += 1;
+	}
+	return 0;
+}
+
 void handle_client(int client_socket) {
 	char buffer[BUFFER_SIZE];
 	int read_socket = 0;
 
 	read_socket = read(client_socket, buffer, BUFFER_SIZE);
 	printf("buffer: \n%s", buffer);
-
+	
+	RequestLine request_line;
+	parseRequestLine(buffer, &request_line);
+	printf("Method: %.*s\n", request_line.method.len, request_line.method.start);
+	printf("Path: %.*s\n", request_line.path.len, request_line.path.start);
+	printf("Version: %.*s\n", request_line.version.len, request_line.version.start);
 }
--- src/server.h
+++ src/server.h
@@ -1,6 +1,18 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+typedef struct {
+	char *start;
+	int len;
+} Slice;
+
+typedef struct {
+	Slice method;
+	Slice path;
+	Slice version;
+} RequestLine;
+
+int parseRequestLine(char *, RequestLine *);
 void handle_client(int);
 
 #endif
